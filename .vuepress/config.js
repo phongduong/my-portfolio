14e3cf78e31eeccc9771d8f270a23321a02f69d0
phongduong <s2pkmol@gmail.com>
2018-09-07 22:20:43 +0700

    Update cms

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,5 +2,11 @@ module.exports = {
   title: "Phong Duong",
   description: "Self-taught developer, interested in learning new things",
   base: "/",
-  serviceWorker: true
+  serviceWorker: true,
+  head: [
+    [
+      "script",
+      { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" }
+    ]
+  ]
 };
