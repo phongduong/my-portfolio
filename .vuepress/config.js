b41e8ce33fced749f81d839c8aff5d9907fc8125
phongduong <phongduong1710@gmail.com>
2018-09-10 17:29:18 +0700

    Update sw

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Phong Duong",
   description: "Self-taught, full-stack developer",
   base: "/",
-  // serviceWorker: true,
+  serviceWorker: true,
   head: [
     [
       "link",
