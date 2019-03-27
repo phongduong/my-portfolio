974ffeca14c856ac769ba22fe64f19ace0df5c56
phongduong <s2pkmol@gmail.com>
2019-03-27 20:04:51 +0700

    Update logo

--- portfolio/.vuepress/config.js
+++ portfolio/.vuepress/config.js
@@ -1,9 +1,50 @@
 module.exports = {
   title: "Phong Duong",
   description:
-    "I am a/an 👨‍💻Coder👨‍💻 ✈️Traveller✈️ 👨‍🌾Farmer👨‍🌾 💰Entrepreneur💰. I also like sport and books.",
+    "I am a/an 👨‍💻Coder👨‍💻 ✈️Traveller✈️ 👨‍🌾Gardener👨‍🌾 💰Entrepreneur💰. I also like sport and books.",
   base: "/",
   serviceWorker: false,
   ga: "UA-123178606-2",
-  head: []
+  head: [
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css?family=Roboto",
+        rel: "stylesheet"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png"
+      }
+    ],
+    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+    [
+      "link",
+      { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#00949c" }
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#00949c" }],
+    ["meta", { name: "theme-color", content: "#00949c" }]
+  ]
 };
