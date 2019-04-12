5ff0ac6adae2b1573305a522d9539a54f04762a1
phong <s2pkmol@gmail.com>
2019-04-12 14:19:32 +0700

    Update

---
+++ portfolio/.vuepress/components/Pages/Post.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>{{ $site }}</div>
+</template>
---
+++ portfolio/.vuepress/theme/Layout.vue
@@ -0,0 +1,34 @@
+<template>
+  <main class="section is-main">
+    <div class="columns">
+      <section class="column is-9">
+        <div class="section box">
+          <Content/>
+        </div>
+      </section>
+      <section class="column is-3">
+        <div class="section box">
+          <Sidebar/>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+import bulma from "bulma";
+import Sidebar from "../components/Sidebar.vue";
+
+export default {
+  components: {
+    Sidebar
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.is-main {
+  background: #00949c;
+}
+</style>
+
