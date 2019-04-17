85c4cf013397c5cedb5d3c24008744540f927f0d
phongduong <s2pkmol@gmail.com>
2019-04-17 22:13:41 +0700

    Update cms

--- portfolio/.vuepress/public/admin/createWidget.js
+++ portfolio/.vuepress/public/admin/createWidget.js
@@ -1,22 +1,28 @@
 var CategoriesControl = createClass({
   handleChange: function(e) {
-    this.props.onChange(e.target.value.split(',').map((e) => e.trim()));
+    this.props.onChange(e.target.value.split(",").map(e => e.trim()));
   },
 
   render: function() {
     var value = this.props.value;
-    return h('input', { type: 'text', value: value ? value.join(', ') : '', onChange: this.handleChange });
+    return h("input", {
+      type: "text",
+      value: value ? value.join(", ") : "",
+      onChange: this.handleChange
+    });
   }
 });
 
 var CategoriesPreview = createClass({
   render: function() {
-    return h('ul', {},
+    return h(
+      "ul",
+      {},
       this.props.value.map(function(val, index) {
-        return h('li', {key: index}, val);
+        return h("li", { key: index }, val);
       })
     );
   }
 });
 
-CMS.registerWidget('categories', CategoriesControl, CategoriesPreview);
+CMS.registerWidget("categories", CategoriesControl, CategoriesPreview);
