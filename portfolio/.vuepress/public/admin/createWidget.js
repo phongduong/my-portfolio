51a9b7809b2ee48d13e1705e8fd65744814e582d
phongduong <s2pkmol@gmail.com>
2019-04-18 13:06:28 +0700

    Update

--- portfolio/.vuepress/public/admin/createWidget.js
+++ portfolio/.vuepress/public/admin/createWidget.js
@@ -1,13 +1,13 @@
 var CategoriesControl = createClass({
   handleChange: function(e) {
-    this.props.onChange(e.target.value.split(",").map(e => e.trim()));
+    this.props.onChange(e.target.value.split(" ").map(e => e.trim()));
   },
 
   render: function() {
     var value = this.props.value;
     return h("input", {
       type: "text",
-      value: value ? value.join(", ") : "",
+      value: value ? value.join(" ") : "",
       onChange: this.handleChange
     });
   }
@@ -25,4 +25,4 @@ var CategoriesPreview = createClass({
   }
 });
 
-CMS.registerWidget("categories", CategoriesControl, CategoriesPreview);
+CMS.registerWidget("meta", CategoriesControl, CategoriesPreview);
