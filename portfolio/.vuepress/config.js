547aafa8262f896b5a044bf83202a61ea805bc6e
Phong Duong <s2pkmol@gmail.com>
2020-02-12 21:16:41 +0700

    update config

--- portfolio/.vuepress/config.js
+++ portfolio/.vuepress/config.js
@@ -7,7 +7,7 @@ const siteConfig = {
 };
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./**/*.vue"],
+  content: ["./**/*.vue", "./**/*.html"],
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 });
 
@@ -26,7 +26,7 @@ module.exports = {
     plugins: [
       require("tailwindcss"),
       require("autoprefixer"),
-      ...(process.env.NODE_ENV !== "development" ? [purgecss] : [])
+      ...(process.env.NODE_ENV !== "production" ? [purgecss] : [])
     ]
   },
   plugins: [
